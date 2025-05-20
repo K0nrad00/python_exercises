cd77abfdb9a486b74dbdb0d08c79c0d400046c96
Konrad Private <K0nrad00@users.noreply.github.com>
2025-05-20 12:07:57 +0100

    Update Day14-higher_lower_game.py

--- 100days/Day14-higher_lower_game.py
+++ 100days/Day14-higher_lower_game.py
@@ -1,54 +1,97 @@
-from game_data import data
-from random import choice
-from art import vs, logo
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-# Get random name, desc and country from data list (A)
-# get another one ^ (B)
-# if user is correct:
-## make B follower an A option for next choice
-## add correct guess to the TOTAL_SCORE (that starts at 0)
-## ask again (loop)
-# if user is wrong - end game (show result of comparison and TOTAL_SCORE)
-def get_followers_details(data_list: list) -> tuple:
-    random_entry = choice(data_list)
-    return random_entry['name'], random_entry['description'], random_entry['country'], random_entry['follower_count']
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
-name_A, description_A, country_A, followers_A  = get_followers_details(data) # unpacking in Python
-name_B, description_B, country_B, followers_B  = get_followers_details(data)
+# Turn off the Coffee Machine by entering “off” to the prompt.
+is_machine_on = True
 
-# ask user for input - compare their followers
-print(logo)
-print(f"Compare A: {name_A}, a {description_A}, from {country_A}.")
-print(vs)
-print(f"Against B: {name_B}, a {description_B}, from {country_B}: ")
-user_choice = input("Who has more followers? type 'a' or 'b': ").lower()
+# check if ingredients_dict for chosen drink are sufficient
+def calculate_resources(user_drink, ingredients_dict):
+    current_resources = {}
+    for res in ingredients_dict:
+        if res in user_drink["ingredients"]:
+            current_resources[res] = ingredients_dict[res] - user_drink["ingredients"][res]
+        else:
+            current_resources[res] = ingredients_dict[res]
+    return current_resources
 
-def is_correct_guess() -> bool:
-    """Check if user guess of followers number is correct"""
-    if followers_A > followers_B and user_choice == "a":
-        return True
-    elif followers_B > followers_A and user_choice == "b":
-        return True
-    return False
 
-total_score = 0
-while is_correct_guess():
-    print("\n" * 20) # clean screen
-    print(logo)
-    total_score +=1
-    print(f"Current score: {total_score}")
-    name_A, description_A, country_A, followers_A = name_B, description_B, country_B, followers_B
-    print(f"Compare A: {name_A}, a {description_A}, from {country_A}.")
-    name_B, description_B, country_B, followers_B = get_followers_details(data)
-    print(vs)
-    print(f"Against B: {name_B}, a {description_B}, from {country_B}. ")
-    # print(name_A, followers_A) # for debugging purpose only
-    # print(name_B, followers_B) # for debugging purpose only
-    user_choice = input("Who has more followers? type 'a' or 'b': ").lower()
-else:
-    print("\n========================================================")
-    print(f"That's not correct, your final score is {total_score},\n"
-          f"The followers number of {name_A} is {followers_A}, followers number of {name_B} is {followers_B}")
+def is_resource_sufficient(user_drink: dict) -> bool:
+    # This does not provide all missing ingredients, just 1st that prevents from making the drink
+    for item in user_drink:
+        if user_drink[item] >= resources[item]:
+            print(f"Sorry, there's not enough {item}")
+            return False
+    return True
 
-# possible enhancement: add chosen A_names to list so they do not appear again
-# refactor : different while loop so the prints are not repeated out of loop
+def process_coins(quarters_amount, dimes_amount, nickles_amount, pennies_amount):
+    total_given = (quarters_amount * 0.25) + (dimes_amount * 0.1) + (nickles_amount * 0.05) + (pennies_amount * 0.01)
+    return total_given
+
+money_in_machine = 0
+calculated_resources = {}
+user_coins = 0
+
+while is_machine_on:
+        user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if user_choice == "espresso" or user_choice == "latte" or user_choice == "cappuccino":
+            chosen_drink_ingredients = MENU[user_choice]
+            # print("Chosen drink", chosen_drink_ingredients)  # DEBUG
+            chosen_drink_cost = MENU[user_choice]['cost']
+            # print("Drink cost", chosen_drink_cost)  # DEBUG
+            # TODO 4: only ask for user_coins if there's enough resources
+            if is_resource_sufficient(chosen_drink_ingredients["ingredients"]):
+                quarters = float(input("Please insert the coins. \nHow many quarters?: "))  # 25c -> 0.25
+                dimes = float(input("How many dimes?: "))  # 10c -> 0.1
+                nickles = float(input("How many nickles: "))  # 5c -> 0.05
+                pennies = float(input("How many pennies: "))  # 1c -> 0.01
+                user_coins = process_coins(quarters, dimes, nickles, pennies)
+                # Check that the user has inserted enough money to purchase the drink they selected.
+                if user_coins < chosen_drink_cost:
+                    print(f"Sorry that's not enough coins. Money refunded: ${user_coins}.")
+                else:
+                    money_in_machine += chosen_drink_cost
+                    user_change = round(user_coins - chosen_drink_cost, 2)
+                    print(f"Here's your change: ${user_change}, Enjoy your drink")
+                    # update the resources each time
+                    calculated_resources = calculate_resources(chosen_drink_ingredients, resources)
+                    resources = calculate_resources(chosen_drink_ingredients,
+                                                    resources)  # udpating resource dict with calculate_resources for next run
+        elif user_choice == "off":
+            print("Powering off..")
+            is_machine_on = False
+        elif user_choice == "report":
+            for resource in resources:
+                print(resource.title(), resources[resource])
+            print(f"Money: ${money_in_machine}", )
+        else:
+            print("I don't know what that is, try again")
+
